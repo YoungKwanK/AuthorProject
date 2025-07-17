8dae440a6ff8af62169911888005db238bda62e0
YoungKwanK <young.kwin@gmail.com>
2025-07-17 09:35:43 +0900

    new project

---
+++ src/main/java/com/beyond/AuthorProject/AuthorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.AuthorProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beyond/AuthorProject/AuthorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.AuthorProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
